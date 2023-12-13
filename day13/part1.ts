98e1eec0b980d5aa311f37bef406f06fee0b3ba2
Kamran Ahmed <kamran322@hotmail.co.uk>
2023-12-13 21:13:44 +0000

    small optimisation update part 1

--- day13/part1.ts
+++ day13/part1.ts
@@ -25,14 +25,14 @@ function findMirrorPosition(grid: string[]) {
     let possibleMirrorPositions: number[] = Array.from({ length: grid[0].length - 1 }, (_, i) => i + 1);
     for (const line of grid) {
         // find vertical reflection
-        possibleMirrorPositions = possibleMirrorPositions.filter(x => getPossibleMirrorPositions(line).has(x));
+        possibleMirrorPositions = possibleMirrorPositions.filter(x => getPossibleMirrorPositions(line, possibleMirrorPositions).has(x));
     }
     return possibleMirrorPositions.at(-1);
 }
 
-function getPossibleMirrorPositions(line: string) {
+function getPossibleMirrorPositions(line: string, availablePositions: number[]) {
     const possibleMirrorPositions = new Set<number>();
-    for (let i = 1; i < line.length; i++) {
+    for (const i of availablePositions) {
         const left = line.slice(0, i);
         const right = line.slice(i);
         if (left.endsWith(reverse(right)) || right.startsWith(reverse(left))) {
